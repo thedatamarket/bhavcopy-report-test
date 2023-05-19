71a2062041d6de18fabf2d547559f41dedf6e65f
The Data Market <110961258+thedatamarket@users.noreply.github.com>
2023-05-19 11:59:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,100 +1,88 @@
-from twython import Twython
-import tweepy
-import requests
-import time
-from datetime import datetime
-import pandas as pd
-import pytz
-import logging
-import logging.handlers
 import os
-
-def msg(message):
-    print(message)
-    
-    bot_token = '5041715929:AAFcraPI9-8jZR0bLkquRDNUXg96tEUKje4'
-    bot_chatID = '1259144189'
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id='+ bot_chatID + '&parse_mode=MarkdownV2&text=' + message
-
-    response = requests.get(send_text)
-    print(response.json())
-    return response.json()
-
-try:
-    CONSUMER_KEY = str(os.environ["CONSUMER_KEY"])
-    CONSUMER_SECRET = str(os.environ["CONSUMER_SECRET"])
-    ACCESS_TOKEN = str(os.environ["ACCESS_TOKEN"])
-    ACCESS_TOKEN_SECRET = str(os.environ["ACCESS_TOKEN_SECRET"])
-except KeyError:
-    msg("Token not available!")
-    #logger.info("Token not available!")
-    #raise
+import pandas as pd
+import numpy as np
+import wget
+import zipfile
+from datetime import datetime, date
 
 
- 
-now = datetime.now()
-current_time = datetime.now(pytz.timezone('Asia/Kolkata'))
-dt = current_time.date()
-filename = "status_" + str(dt) + ".log"
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    filename,
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
+import requests, zipfile, io
+
+def down(URL):
+  try:
+    r = requests.get(URL, verify=False, timeout=3)
+    print(r)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall("Test/")
+    return 1
+  except:
+    return 0
 
 
+year = ['MAY']
+month = ['2023']
+print('downloading......')
+for p in month:
+    for j in year:
+        for i in range(1,31):
+            try:
+                year = p
+                dt = str(i) + j + year
+                # print(dt)
+                if str(dt) == '19MAY2023':
+                    break
+                if i < 10:
+                    str1 = 'cm0' + dt +'bhav.csv.zip'
+                else:
+                    str1 = 'cm' + dt +'bhav.csv.zip'
+                print(str1)
+                URL = 'https://archives.nseindia.com/content/historical/EQUITIES/'+year+'/' + j + '/' + str1
+                # print(URL)
+                resp = down(URL)
+                if resp == 0:
+                  pass
+            except:
+              print("passed")
+              pass
 
+print('processing.....')
+path = "Test"
+dir_list = sorted(os.listdir(path))
+dir, dir1, dir2 = [], [], []
+for i in range(len(dir_list)):
+  dir.append(dir_list[i].split('cm')[1].split('bhav')[0])
+  datetime_object = datetime.strptime(dir[i], '%d%b%Y')
+  dir1.append(datetime_object.date())
 
-start = 0
-try:
-    if start == 0:
-        twitter = Twython(
-            CONSUMER_KEY,
-            CONSUMER_SECRET,
-            ACCESS_TOKEN,
-            ACCESS_TOKEN_SECRET
-        )
-        # authorization of consumer key and consumer secret
-        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-        
-        # set access to user's access key and access secret 
-        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-        
-        # calling the api 
-        api = tweepy.API(auth)
-        
-        # WOEID of London
-        woeid_ind = 23424848
-        
-        # fetching the trends
-        trends = api.trends_place(id = woeid_ind)
-        
-        # printing the information
-        print("The top trends for the location are :")
-        trendLst = []
+dir1 = sorted(dir1)
+for i in range(len(dir1)):
+  d = dir1[i].strftime("%d%b%Y")
+  fin = 'cm'+d.upper()+'bhav.csv'
+  dir2.append(fin)
+dir_list = dir2
+dir_list.reverse()
+ticker = pd.read_csv("ticker.csv")
+ticker = ticker[["SYMBOL"]]
+df2 = ticker.merge(ticker, on='SYMBOL', how='left')
+for i in dir_list:
+  path = "Test/"+str(i)
+  prevClose = 'PREVCLOSE_'+str(i)
+  close = 'CLOSE_'+str(i)
+  df = pd.read_csv(path)
+  df = df[df['SERIES'] == "EQ"]
+  df.rename(columns = {'PREVCLOSE':prevClose, 'CLOSE':close}, inplace = True)
+  df[i] = ((df[close] - df[prevClose])/df[prevClose]) * 100
+  df = df[["SYMBOL", i]]
+  df2 = df2.merge(df, on='SYMBOL', how='left')
 
-        print("The top trends for the location are :")
-        trendLst = []
-        for value in trends:
-            for trend in value['trends']:
-                trendLst.append(trend['name'])
-                now = datetime.now()
-                current_time = datetime.now(pytz.timezone('Asia/Kolkata'))
-                print(current_time.date(),current_time.time(),trend['name'],trend['tweet_volume'])
-                c_date = str(current_time.date())
-                c_time = current_time.time()
-                trending = trend['name']
-                volume = trend['tweet_volume']
+#save file
+today = date.today()
+d4 = today.strftime('%b-%d-%Y')
+filename = str(d4) + '.csv'
+df2.to_csv(filename)
+print('Process completed... file generated ', filename)
 
-                logger.info(f'{c_date},{c_time},{trending},{volume}')
-except Exception as e:
-  print(e)
-  msg("Process Stopped Need your attention")
-
-
+#add market cap at column
+#add current price column
+#52weeks high and low col
+#type of industry
